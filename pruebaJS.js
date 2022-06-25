e9800986a0d8f5e2c7d7b1e0d64253e418c8dc5d
Diego Montoya <dmontoyg@gmail.com>
2022-06-25 17:33:24 -0500

    refuerzo teoria js

--- pruebaJS.js
+++ pruebaJS.js
@@ -1,1 +1,54 @@
-/* Prueba escrita de los conocimientos previos  */
+/*
+
+Prueba escrita de los conocimientos previos
+
+1. Responda las siguientes preguntas
+
+¿Qué es una variable y para qué sirve?
+R// Es un campo que se puede denominar, como un contenedor, donde permite almacenar diferentes tipos de datos: string, number, boolean, etc. Posteriormente, podemos acudir a dicho espacio, para consultar, modificar o eliminar.
+
+¿Cuál es la diferencia entre declarar e inicializar una variable?
+R// Declarar, es cuando la variable aun no se ha inicializado, mientras que al momento de inicializarse si, ejemplo:
+
+    let varible; // Estamos declarando
+    variable = true; // Estamos inicializando
+
+¿Cuál es la diferencia entre sumar números y concatenar strings?
+R// 
+
+¿Cuál operador me permite sumar o concatenar?
+
+
+2. Determina el nombre y tipo de dato para almacenar en variables la siguiente información:
+
+Nombre = String
+Apellido = String
+Nombre de usuario en Platzi = String
+Edad = Number
+Correo electrónico = String
+Mayor de edad = Boolean
+Dinero ahorrado = Number
+Deudas = Number
+
+3. Traduce a código JavaScript las variables del ejemplo anterior y deja tu código en los comentarios.
+
+Let nombre = 'Diego';
+Let apellido = 'Montoya';
+Let userPlatzi = 'dmontoyg';
+Let mayorEdad = true;
+Let email = 'dmontoyg@gmail.com';
+Let dineroAhorrado = 2000;
+Let deudas = null;
+
+4. Calcula e imprime las siguientes variables a partir de las variables del ejemplo anterior:
+
+Nombre completo (nombre y apellido)
+Dinero real (dinero ahorrado menos deudas)
+
+
+
+*/
+
+//PENDIENTES
+
+IMPORTANTE
