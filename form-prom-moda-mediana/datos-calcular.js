46db8f73a401bce46b8c6f53a32beb02a4fa2d82
Diego Montoya <dmontoyg@gmail.com>
2022-10-09 12:16:33 -0500

    creacion del promedio

---
+++ form-prom-moda-mediana/datos-calcular.js
@@ -0,0 +1,14 @@
+const lista1 = [
+    100,
+    200,
+    300,
+    500,
+];
+
+let SumaLista1 = 0;
+
+for (let i = 0; i < lista1.length; i++) {
+    SumaLista1 = SumaLista1 + lista1[i];
+}
+
+const promedioLista1 = SumaLista1 / lista1.length;
