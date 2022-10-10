ea2bb450dd70542978a969b0dcf400f48ec33076
Diego Montoya <dmontoyg@gmail.com>
2022-10-09 20:05:01 -0500

    2da forma de hacer la funcion

--- form-prom-moda-mediana/datos-calcular.js
+++ form-prom-moda-mediana/datos-calcular.js
@@ -1,14 +1,12 @@
-const lista1 = [
-    100,
-    200,
-    300,
-    500,
-];
 
-let SumaLista1 = 0;
+function calcularMediaAritmetica(lista){
+//    let sumaLista = 0;
+//    for (let i = 0; i < lista.length; i++){
+//        sumaLista = sumaLista + lista[i];
+//   }
 
-for (let i = 0; i < lista1.length; i++) {
-    SumaLista1 = SumaLista1 + lista1[i];
+const sumaLista = lista.reduce();
+const promedioLista = sumaLista / lista.length;
+
+    return promedioLista;
 }
-
-const promedioLista1 = SumaLista1 / lista1.length;
